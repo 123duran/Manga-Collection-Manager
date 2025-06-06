7492bb92e5f91ba2eec0474dcbbc8c58c985e3a8
Bruno Duran <bgdbruno@gmail.com>
2025-06-06 13:26:53 -0300

    feat:  adding templates and paths

--- mangasV2/mangas/urls.py
+++ mangasV2/mangas/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.manga_list, name='manga_list'),  # List all mangas
+    path('<int:manga_id>/', views.manga_detail, name='manga_detail'),  # Manga Details
+    path('chapter/<int:id>/toggle-owned/', views.toggle_owned_chapter, name='toggle_owned_chapter'),  # Toggle ownership
+    path('manga/<int:manga_id>/create-chapter/', views.create_chapter, name='create_chapter'),  # Create chapter
 ]
