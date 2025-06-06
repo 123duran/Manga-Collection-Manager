c4f7e09dcf05ebf448ecaaf1665b6bbf0cdaeffc
Bruno Duran <bgdbruno@gmail.com>
2025-06-06 13:21:52 -0300

    chore: register Manga and Chapter models in Django admin
    - Added MangaAdmin and ChapterAdmin classes for better admin interface
    - Configured list display, search fields, and filters for both models

--- mangasV2/mangas/admin.py
+++ mangasV2/mangas/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Manga, Chapter
+
+@admin.register(Manga)
+class MangaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'volumes', 'release_date', 'date_added')
+    search_fields = ('title', 'author')
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('manga', 'number', 'owned', 'date_acquired')
+    list_filter = ('owned', 'manga')
